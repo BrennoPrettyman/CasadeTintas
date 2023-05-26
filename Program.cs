3c66d5f72dcec42df626ca8feb7fa6bf156b0daa
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-05-26 20:34:39 -0300

    Resolvido

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+
+Console.WriteLine("***Casa de Tintas***\n");
+Console.Write("Digite o tamanho da área....: ");
+
+double area;
+
+area = Convert.ToDouble(Console.ReadLine());
+
+
+double TotalLitros = (area/3)*110/100;
+
+Console.WriteLine($"O tamanho de sua área é {TotalLitros} m²");
+
+double TotalLatas;
+double TotalGaloes;
+
+int Lata = 18;
+
+TotalLatas = TotalLitros/Lata;
+
+double Galoes = 3.6;
+
+TotalGaloes = TotalLitros/Galoes;
+
+Console.Write($"O total de latas é {TotalLatas:N0} e o total de galões é {TotalGaloes:N0}");
